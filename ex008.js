040ea73d50d03421e55cb1bcf335892462b2f829
Gustavo Henrique Rambo <gustavohrambo@gmail.com>
2023-01-23 19:34:49 -0300

    update

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou o límite de velocidade e está multado!')
+} else {
+    console.log('Você dirige com responsabilidade')
+}
+console.log('Dirija sempre usando cinto de segurança')
