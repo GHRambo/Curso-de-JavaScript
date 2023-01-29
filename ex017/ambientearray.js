b754600b980bb3db371dc9e6fdb88a51c02c5a75
Gustavo Henrique Rambo <gustavohrambo@gmail.com>
2023-01-29 16:31:26 -0300

    Update ambientearray.js

--- ex017/ambientearray.js
+++ ex017/ambientearray.js
@@ -6,7 +6,14 @@ console.log(num)
 console.log(`O Vetor tem ${num.length} posições`)
 console.log(`O segundo valor do vetor é ${num[1]}`)
 console.log(num.sort())
-
-for (var c = 0; c <= (num.length); c++) {
+0
+for (var c = 0; c <= num.length; c++) {
     console.log(num[c])
 }
+
+for ( var c in num){
+    console.log(num[c])
+}
+
+console.log(`O valor 7 se encontra na posição número ${num.indexOf(7)}`)
+console.log(num.indexOf(11))// Quando não encontra o valor o JS retorna -1
