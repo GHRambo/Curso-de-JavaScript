d2f8cfa89188abced68ceeabbb08b2de5ceeb161
Gustavo Henrique Rambo <gustavohrambo@gmail.com>
2023-01-29 14:25:22 -0300

    finish

--- ex016/script.js
+++ ex016/script.js
@@ -5,21 +5,29 @@ function contar() {
     let res = document.getElementById('res')
 
     if ( ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Faltam dados!')
+        //window.alert('[ERRO] Faltam dados!')
     }else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>' 
         let i = Number(ini.value)
         let f = Number(fim.value)
-        let p = Number(passo.valeu)
-
-        for (let c = i; c <= f; c += p){
-            res.innerHTML += `${c}`
-            console.log(i)
-            console.log(f)
-            console.log(p)
-        
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
-
+        // Contagem crescente
+        if (i < f){
+            for (let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`      
+            }
+            res.innerHTML += `\u{1F3C1}`
+        }else { //Contagem regressiva
+            for ( let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+        }
+        
     }
     
 }
